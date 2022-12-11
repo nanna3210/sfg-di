bbf8f0f647475aa0b0e3f1db26c69492e4dddfc5
nanna3210 <navkishoredas@gmail.com>
2022-12-11 17:03:47 +0530

    1st

---
+++ src/main/java/com/nanna/sfgdi/SfgdiApplication.java
@@ -0,0 +1,13 @@
+package com.nanna.sfgdi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgdiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfgdiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nanna/sfgdi/SfgdiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nanna.sfgdi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgdiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
