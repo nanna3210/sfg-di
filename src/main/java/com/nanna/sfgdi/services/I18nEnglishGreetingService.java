f560de51cbaf05245e5459d93f936b762b5f636c
nanna3210 <navkishoredas@gmail.com>
2022-12-13 23:26:31 +0530

    @profiles

--- src/main/java/com/nanna/sfgdi/services/I18nEnglishGreetingService.java
+++ src/main/java/com/nanna/sfgdi/services/I18nEnglishGreetingService.java
@@ -3,6 +3,7 @@ package com.nanna.sfgdi.services;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 @Profile ( "EN" )
+//profiles Are mentioned in application .properties for the bootstrap
 @Service("i18nservice")
 public class I18nEnglishGreetingService implements GreetingService{
 
